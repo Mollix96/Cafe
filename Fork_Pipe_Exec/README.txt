18c68021cecc7e58d0c4159460ae743634bbc9a4
Mirco Molinari <mollix96@gmail.com>
2019-11-24 19:25:02 +0100

    fork_pipe_exec

---
+++ Fork_Pipe_Exec/README.txt
@@ -0,0 +1,7 @@
+Esempio di fork + exec
+
+Esegue il comando Unix:
+> echo "Hello, World" | cut -f 1 -d ,
+
+Che produce come output:
+Hello
---
+++ Fork_Pipe_Exec/cut_echo.c
@@ -0,0 +1,76 @@
+#include <fcntl.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+/* Gesisce i figli terminati */
+void handler(int signo)
+{
+    int status;
+
+    while (waitpid(-1, &status, WNOHANG) > 0)
+        ;
+}
+
+int main(void)
+{
+    pid_t pid; /* Process ID */
+    int status;
+    int n1p1[2];
+
+    /* Evita processi zombie */
+    signal(SIGCHLD, handler);
+
+    pid = fork();
+    if (pid < 0)
+    {
+        perror("fork");
+        exit(EXIT_FAILURE);
+    }
+    else if (pid == 0)
+    {
+        /* Figlio */
+        pipe(n1p1);
+        pid = fork();
+        if (pid < 0)
+        {
+            perror("fork");
+            exit(EXIT_FAILURE);
+        }
+        if (pid == 0)
+        {
+            /* Nipote */
+            close(0);
+            dup(n1p1[0]);
+            close(n1p1[0]);
+            close(n1p1[1]);
+
+            /* SECONDO comando */
+            execlp("cut", "cut", "-f", "1", "-d", ",", NULL);
+            perror("exec");
+            exit(EXIT_FAILURE);
+        }
+        else
+        {
+            /* Figlio */
+            close(1);
+            dup(n1p1[1]);
+            close(n1p1[0]);
+            close(n1p1[1]);
+
+            /* PRIMO comando */
+            execlp("echo", "echo", "Hello, World!", NULL);
+
+            perror("exec");
+            exit(EXIT_FAILURE);
+        }
+    }
+    else
+    {
+        /* Genitore */
+        wait(&status);
+    }
+}
